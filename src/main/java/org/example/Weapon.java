92bc2536c55c276aedffb9b915a029354c9e1f7b
Elif Cetin <xxelifcetinxx@gmail.com>
2025-05-08 13:43:10 +0300

    review-weapon class modified

--- src/main/java/org/example/Weapon.java
+++ src/main/java/org/example/Weapon.java
@@ -1,26 +1,23 @@
-
 package org.example;
 
 public enum Weapon {
-    SWORD(15, 1.2),
-    DAGGER(8, 2.5),
-    AXE(20, 0.9);
+    SWORD(50, 1.5),
+    AXE(60, 1.2),
+    BOW(30, 2.0);
 
     private final int damage;
     private final double attackSpeed;
 
-    // Constructor for enum constants
     Weapon(int damage, double attackSpeed) {
         this.damage = damage;
         this.attackSpeed = attackSpeed;
     }
 
-    // Getters for enum fields
-    public int getDamage() {
+    public Integer getDamage() {
         return damage;
     }
 
-    public double getAttackSpeed() {
+    public Double getAttackSpeed() {
         return attackSpeed;
     }
 }
